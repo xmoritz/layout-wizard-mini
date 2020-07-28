0292432dddc8194322d83614eab3fdb13a06f992
xmoritz <m.scharnhop@sp.design>
2020-07-29 00:15:10 +0200

    refactor(gh-pages): add build files

---
+++ precache-manifest.0b7bfbb4c6890940550f7e05604a301e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56c80f3d14394f0a56646dd7032c4ddc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a67cb5ad0298e4f6b0f",
+    "url": "/static/css/main.9aa157ac.chunk.css"
+  },
+  {
+    "revision": "1f4daa3782a02ae9d0ba",
+    "url": "/static/js/2.f04774ec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f04774ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a67cb5ad0298e4f6b0f",
+    "url": "/static/js/main.abd5de1b.chunk.js"
+  },
+  {
+    "revision": "fd389284c81dcf5a6a08",
+    "url": "/static/js/runtime-main.1558b25b.js"
+  },
+  {
+    "revision": "9b30e61246b9599a6b74cdb66c39d908",
+    "url": "/static/media/wizard.9b30e612.png"
+  }
+]);
