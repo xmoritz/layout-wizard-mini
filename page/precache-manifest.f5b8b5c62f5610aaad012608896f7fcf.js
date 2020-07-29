7d457afac68689f917ff98df93a0d89fa50a3b03
xmoritz <m.scharnhop@sp.design>
2020-07-29 13:00:28 +0200

    refactor(): add homepage path

---
+++ page/precache-manifest.f5b8b5c62f5610aaad012608896f7fcf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "521c46156a2eaf37e4c1b1cab4a70596",
+    "url": "./index.html"
+  },
+  {
+    "revision": "98237c66eda6fcb50e62",
+    "url": "./static/css/main.9aa157ac.chunk.css"
+  },
+  {
+    "revision": "1f4daa3782a02ae9d0ba",
+    "url": "./static/js/2.f04774ec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.f04774ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98237c66eda6fcb50e62",
+    "url": "./static/js/main.6213b88c.chunk.js"
+  },
+  {
+    "revision": "8ec8ed5cd5912cd81370",
+    "url": "./static/js/runtime-main.93c90f5f.js"
+  },
+  {
+    "revision": "9b30e61246b9599a6b74cdb66c39d908",
+    "url": "./static/media/wizard.9b30e612.png"
+  }
+]);
--- page/service-worker.js
+++ page/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0b7bfbb4c6890940550f7e05604a301e.js"
+  "./precache-manifest.f5b8b5c62f5610aaad012608896f7fcf.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
